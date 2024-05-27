732b2dd8e42fe69b6bb32e83b23bce306bc900a4
RefubuS <refubuspl@gmail.com>
2024-05-28 01:35:13 +0200

    Commit no. 1

---
+++ authentication.js
@@ -0,0 +1,41 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+import { firebaseConfig } from "./firebaseConfig";
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+
+const signInButton = document.querySelector("#signInButton");
+const signOutButton = document.querySelector("#signOutButton");
+
+const userSignIn = async () => {
+    signInWithPopup(auth, provider).then((result) => {
+        const user = result.user;
+        console.log(user);
+    }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+    })
+}
+
+const userSignOut = async () => {
+    signOut(auth).then(() => {
+        alert("You have been signed out!")
+    }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+    })
+}
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        alert("You are authenticated with Google");
+        console.log(user);
+    }
+})
+
+signInButton.addEventListener("click", userSignIn);
+signOutButton.addEventListener("click", userSignOut);
